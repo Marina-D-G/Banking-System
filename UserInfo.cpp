6f840936c33c5e4f01529d2ab9c413195ae2003d
Marina-D-G <147334873+Marina-D-G@users.noreply.github.com>
2024-06-25 02:06:15 +0300

    Delete UserInfo.cpp

--- UserInfo.cpp
+++
@@ -1,9 +0,0 @@
-#include "UserInfo.h"
-
-UserInfo::UserInfo(String& _firstName, String& _lastName, int _egn, int _age)
-{
-	firstName = _firstName;
-	lastName = _lastName;
-	egn = _egn;
-	age = _age;
-}
